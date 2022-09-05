978684a0199affec3c3fe59036bf5f6cccd307a1
introvertCode <stanislaw.bielski.programista@gmail.com>
2022-09-05 21:12:50 +0200

    initial commit

---
+++ src/main/java/pl/pogoda/App.java
@@ -0,0 +1,13 @@
+package pl.pogoda;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
